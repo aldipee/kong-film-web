8987fe14e99eb3b732e83fb5397f51d1fad180b6
Aldi Pranata <aldipeee@gmail.com>
2021-07-20 13:15:02 +0700

    Feat: Add logo

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,13 +1,15 @@
-import React, { PureComponent } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import SearchBox from '../Search';
+import React, { PureComponent } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import SearchBox from "../Search";
 const useStyles = makeStyles((theme) => ({
   heroContent: {
     padding: theme.spacing(3, 0, 6),
     margin: theme.spacing(0, 0, 0),
     height: theme.spacing(60),
+    zIndex: 333,
+    position: "relative",
   },
   input: {
     marginLeft: theme.spacing(1),
@@ -15,9 +17,11 @@ const useStyles = makeStyles((theme) => ({
   },
 
   description: {
-    // color: "#fff",
+    color: "#e0e0e0",
   },
   tagline: {
+    fontWeight: "bold",
+    color: "gold",
     // color: "#fbc02d",
   },
 }));
@@ -25,20 +29,22 @@ const useStyles = makeStyles((theme) => ({
 export default function HeroBanner() {
   const classes = useStyles();
   return (
-    <div className={classes.heroContent}>
-      <Container maxWidth='sm' my={10}>
-        <Typography className={classes.tagline} component='h1' variant='h2' align='center' gutterBottom>
-          IndoXXI Premiere
-        </Typography>
-        <Typography className={classes.tagline} component='h1' variant='h5' align='center' gutterBottom>
-          Exclusive Download and Movies Streaming
-        </Typography>
-        <Typography className={classes.description} variant='body2' align='center' color='textSecondary' paragraph>
-          IndoXXI Premiere adalah layanan berlangganan #1 untuk nonton Film dan TV Series dengan harga termurah Kami
-          menyediakan film, serial, terupdate setiap harinya.
-        </Typography>
-        <SearchBox />
-      </Container>
-    </div>
+    <>
+      <div className={[classes.heroContent]}>
+        <Container maxWidth="sm" my={10}>
+          <Typography className={classes.tagline} component="h1" variant="h2" align="center" gutterBottom>
+            KONG FILM
+          </Typography>
+          <Typography className={classes.tagline} component="h1" variant="h5" align="center" gutterBottom>
+            Exclusive Download and Movies Streaming
+          </Typography>
+          <Typography className={classes.description} variant="body2" align="center" color="textSecondary" paragraph>
+            IndoXXI Premiere adalah layanan berlangganan #1 untuk nonton Film dan TV Series dengan harga termurah Kami
+            menyediakan film, serial, terupdate setiap harinya.
+          </Typography>
+          <SearchBox />
+        </Container>
+      </div>
+    </>
   );
 }
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,30 +1,34 @@
-import React, { PureComponent } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
+import React, { PureComponent } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
 const useStyles = makeStyles((theme) => ({
   root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
     width: 600,
-    boxShadow: '0px 8px 15px rgba(79, 79, 79, 0.25)',
-    borderRadius: '2px',
+    boxShadow: "0px 8px 15px rgba(0, 0, 0, 0.4)",
+    borderRadius: "2px",
+    background: "#333333",
   },
 
   input: {
     marginLeft: theme.spacing(1),
     flex: 1,
+    color: "#b5b5b5",
   },
   iconButton: {
     padding: 10,
+    color: "#b5b5b5",
   },
   divider: {
     height: 28,
     margin: 4,
+    backgroundColor: "#b5b5b5",
   },
 }));
 
@@ -35,12 +39,12 @@ export default function HeroBanner() {
       <Paper className={classes.root}>
         <InputBase
           className={classes.input}
-          placeholder='Search Google Maps'
-          inputProps={{ 'aria-label': 'search google maps' }}
+          placeholder="Cari judul film"
+          inputProps={{ "aria-label": "search google maps" }}
         />
 
-        <Divider className={classes.divider} orientation='vertical' />
-        <IconButton type='submit' className={classes.iconButton} aria-label='search'>
+        <Divider className={classes.divider} orientation="vertical" />
+        <IconButton type="submit" className={classes.iconButton} aria-label="search">
           <SearchIcon />
         </IconButton>
       </Paper>
